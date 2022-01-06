a298838af53a8479c98ca68c4e152edf0558163c
Pavel Solovev <pavelsol1990@gmail.com>
2022-01-06 12:49:28 +0300

    JS_TS-for-students

---
+++ src/02/02.test.ts
@@ -0,0 +1,15 @@
+let city: CityType;
+
+beforeEach (() => {
+    city = {
+        title: 'New York',
+        houses: [],
+        governmentBuildings: [],
+        citizensNumber: 1000000
+    }
+})
+
+test('city contains 3 houses', () => {...})
+
+
+test.skip('city contain hosp & fire station')
--- src/index.tsx
+++ src/index.tsx
@@ -15,3 +15,8 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//запилить объекты, мб с массссивами и подъобъектами и их типизация
+//
+//
+//
